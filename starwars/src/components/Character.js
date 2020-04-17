8d4b08626cbc99bed0078223053669cb1dac24e5
Chris Forrest <redrider_cr@yahoo.com>
2020-04-17 13:48:50 -0400

    Simplified my layout got rid of CharacterCard.js moved the data into Character.js had to install styled-components as well but mvp met

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,15 +2,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Character = () => {
 
+const Character = ({ character }) => {
+    const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid rgb(210, 210, 210);
+    border-radius: 5px;
+    box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
+    margin: 8px;
+    padding: 12px;
+    background-color: white;
+    width: 50%;
+    `
+    const CharacterCardTitle = styled.h2`
+    width: 100%;
+    word-break: break-word;
+    `
 
-
-    return(
-
-        <div></div>
-    )
-
-}
-
-export default Character;
+    return (
+        <Container>
+          <CharacterCardTitle>{character.name}</CharacterCardTitle>
+          <p>Gender: {character.gender}</p>
+          <p>Birth Year: {character.birth_year}</p>
+          <p>Height: {character.height}</p>
+          <p>Mass: {character.mass}</p>
+        </Container>
+      );
+    };
+    
+    export default Character
